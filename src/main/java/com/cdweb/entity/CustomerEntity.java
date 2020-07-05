8e6279bcf854ec697ff5b6c87c0344b241af3561
DO DUY KHANG <doduykhang98@gmail.com>
2020-07-05 10:15:08 +0700

    thu

--- src/main/java/com/cdweb/entity/CustomerEntity.java
+++ src/main/java/com/cdweb/entity/CustomerEntity.java
@@ -77,7 +77,7 @@ public class CustomerEntity extends BaseEntity{
 	}
 
 	public void setRentDetailEntities(Set<RentDetailEntity> rentDetailEntities) {
-		RentDetailEntities = rentDetailEntities_Khang;
+		RentDetailEntities = rentDetailEntities;
 	}
 //Khang nè
 }	
